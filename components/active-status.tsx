365def6c7c18db7b316a4a19991808da796af524
Serkan <serkanbay02@hotmail.com>
2025-03-25 09:45:50 +0300

    Hearth attack effect on status indicator

--- components/active-status.tsx
+++ components/active-status.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/cn";
 import { Await, useLoaderData } from "@tanstack/react-router";
+import { motion } from "motion/react";
 
 export function ActiveStatus() {
   const { statusPromise } = useLoaderData({ from: "/_app/" });
@@ -15,12 +16,31 @@ export function ActiveStatus() {
 }
 
 function Status({ status }: { status: string }) {
+  const isActive = status.toLowerCase() === "active";
+
   return (
-    <div
+    <motion.div
       title={status}
-      className={cn("h-2 w-2 rounded-full bg-red-600", {
-        "bg-green-600": status.toLowerCase() === "active",
-      })}
-    ></div>
+      className={cn(
+        "relative flex h-2 w-2 items-center justify-center rounded-full bg-red-600",
+        {
+          "bg-green-600": isActive,
+        },
+      )}
+    >
+      {isActive && (
+        <motion.div
+          initial={{ width: 8, height: 8, opacity: 1 }}
+          animate={{ width: 24, height: 24, opacity: 0 }}
+          transition={{
+            ease: "easeInOut",
+            duration: 1,
+            repeat: Infinity,
+            delay: 1.6,
+          }}
+          className="absolute rounded-full bg-green-600/70"
+        ></motion.div>
+      )}
+    </motion.div>
   );
 }
